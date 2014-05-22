307f655b887a5a7edf1ae705c9d5c16eefbe2f67
604-05 <604-05@604-05.labif.local>
2014-05-22 15:39:06 +0700

    First Commit

---
+++ src/main/java/com/mycompany/ujianrkppl123040453/Soal.java
@@ -0,0 +1,29 @@
+package com.mycompany.ujianrkppl123040453;
+
+/**
+ * Hello world!
+ *
+ */
+public class Soal 
+{
+   public int pangkat (int bil, int pangkat) {
+       int hasil = 0;
+       int n = 1;
+       while (n <= pangkat){
+           n ++;
+           hasil *=bil;
+       }
+       return hasil;
+   }
+   public int faktorial(int bil){
+       int hasil = 1;
+       for(int i=1;i<=bil;i++){
+           hasil *=i;
+       }
+       return hasil;
+   }
+   public int perkalian(int bilA, int bilB){
+       return bilA*bilB;
+   }
+   
+}
